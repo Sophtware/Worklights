a29b10d6f3fd5f879d61ce789075ec457b110829
Maggie Yu <maggieyu@cmu.edu>
2016-09-20 21:37:14 -0400

    Added jquery CDN link

--- popup.js
+++ popup.js
@@ -8,15 +8,14 @@ var insertionCode = 'function tint(){\
 tint();'
 
 function click(e) {
-//  chrome.tabs.executeScript(null, {file: chrome.extension.getURL("js/tint.js")});
-  chrome.tabs.executeScript(null, {code: insertionCode});
+    chrome.tabs.executeScript(null, {code: insertionCode});
     window.close();
 }
 
 
 document.addEventListener('DOMContentLoaded', function () {
-  var divs = document.querySelectorAll('div');
-  for (var i = 0; i < divs.length; i++) {
-    divs[i].addEventListener('click', click);
-  }
+    var divs = document.querySelectorAll('div');
+    for (var i = 0; i < divs.length; i++) {
+        divs[i].addEventListener('click', click);
+    }
 });
