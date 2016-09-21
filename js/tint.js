dcad16e7d9d21abfa52bfd990054e3a734374f1f
Maggie Yu <maggieyu@cmu.edu>
2016-09-20 21:59:03 -0400

    Removed unnecessary js file

--- js/tint.js
+++
@@ -1,7 +0,0 @@
-function tint(){
-  var className = "tint";
-  var regex = new RegExp("(^"+className+")|( "+className+")");
-  if(! document.body.className.match(regex) ){
-    document.body.className += " " + className;
-  }
-}
