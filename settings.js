b18913a4c2ff5f68ac4941ffb6548bb64318d224
Maggie Yu <maggieyu@cmu.edu>
2016-09-20 22:59:45 -0400

    Added materialize to settings page

--- settings.js
+++ settings.js
@@ -1,11 +1,23 @@
-var opts = {
-  type: "basic",
-  title: "Worklights Reminder",
-  message: "You have finished your current routine. Start another default routine?",
-  iconUrl: "icon.png",
-  buttons: [{title: "Yes"}, {title: "No"}]
+var notification-options = {
+    type: "basic",
+    title: "Worklights Reminder",
+    message: "You have finished your current routine. Start another default routine?",
+    iconUrl: "icon.png",
+    buttons: [{title: "Yes"}, {title: "No"}]
+}
+
+function click(e) {
+    var elementId = this.id;
+    if (elementId == "show-notification") {
+        chrome.notifications.create(notification-options, function(){});
+    }
+    
+    window.close();
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    chrome.notifications.create("test", opts, function(){});
+    var divs = document.querySelectorAll('div');
+    for (var i = 0; i < divs.length; i++) {
+        divs[i].addEventListener('click', click);
+    }
 });
